1d39d8562cd4cd44f8bf15e54299a4b1ce164774
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-12 11:15:42 -0700

    Converted the static html pages to handlebar files and added the routes and controllers or the various pages

---
+++ app_server/controllers/contact.js
@@ -0,0 +1,8 @@
+/* GET Homepage */
+const contact = (req, res) => {
+    res.render('contact', {title: "Travlr Getaways"})
+}
+
+module.exports = {
+    contact
+}
---
+++ app_server/controllers/meals.js
@@ -0,0 +1,8 @@
+/* GET Homepage */
+const meals = (req, res) => {
+    res.render('meals', {title: "Travlr Getaways"})
+}
+
+module.exports = {
+    meals
+}
