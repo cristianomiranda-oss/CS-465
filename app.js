2c4719cbf5a119b7aed7712c54310124344cb496
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-16 13:08:53 -0700

    Module 3 completed baseline

--- app.js
+++ app.js
@@ -26,7 +26,8 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+// Setts the index option to prevent the public index.html file from being served as the home page 
+app.use(express.static(path.join(__dirname, "public"), {index: false}));
 
 
 app.use("/", indexRouter);
