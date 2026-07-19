68acc195e15519679e4dbb177b794424989fa13b
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-19 10:06:50 -0700

    Created the script to seed the trips collection in the database

---
+++ app_server/models/seed.js
@@ -0,0 +1,23 @@
+// Imports the DB connection and Trip schema
+const Mongoose = require('./db');
+const Trip = require('./travlr');
+
+// Reads seed data from the appropriate json file
+var fs = require('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf-8'));
+
+// Deletes any existing records and inserts the seed data
+const seedDB = async () => {
+    // Deletes all records
+    await Trip.deleteMany({});
+    // Inserts seed data
+    await Trip.insertMany(trips);
+}
+
+//Closes the MongoDB connection and exits
+seedDB.then(async () => {
+    // Closes connection
+    await Mongoose.connection.close();
+    // Exits the process
+    process.exit(0);
+})
