ae3da4ee3ce764765c40735fe580bbf111625164
cristiano-oss <cristiano.mirande@snhu.edu>
2026-07-26 11:46:07 -0700

    Added the search by code api endpoint

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -20,6 +20,25 @@ const tripsList = async (req, res) => {
   }
 };
 
+// GET: /trips/:tripCode - lists a single trip
+// Regardless of the outcome, response includes an HTML status code and a JSON message to the requesting client
+const tripsFindByCode = async (req, res) => {
+  // Filters for a specific trip based on its code
+  const q = await Model.find({'code': req.params.tripCode}).exec();
+
+  // DEBUG: shows the results of the query
+  //   console.log(q);
+
+  if (!q) {
+    // Database returned no data
+    return res.status(404).json(err);
+  } else {
+    // Returns resulting trips list
+    return res.status(200).json(q);
+  }
+};
+
 module.exports = {
-    tripsList
+    tripsList,
+    tripsFindByCode
 }
--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+// IMports the trips controller
 const tripsController = require("../controllers/trips");
 
+// Defines routes for trip endpoints
 router.route("/trips").get(tripsController.tripsList);
+router.route("/trips/:tripCode").get(tripsController.tripsFindByCode);
 
 module.exports = router;
