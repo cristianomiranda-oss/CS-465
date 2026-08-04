c3c469ec06d1da5b61721fdc2a862a42ab45d497
cristiano-oss <cristiano.mirande@snhu.edu>
2026-08-04 11:50:31 -0700

    Fixed comment error

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -68,8 +68,7 @@ const tripsAddTrip = async (req, res) => {
 };
 
 // PUT: /trips/:tripCode - Adds a new Trip
-// Regardless of outcome, response must include HTML status
-code;
+// Regardless of outcome, response must include HTML status code
 // and JSON message to the requesting client
 const tripsUpdateTrip = async (req, res) => {
   // Uncomment for debugging
