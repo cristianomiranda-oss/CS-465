c11ee2fda5489ded76856f3bf7a061417638161e
cristiano-oss <cristiano.mirande@snhu.edu>
2026-08-04 10:57:12 -0700

    Added bootstrap and other modules

--- app_admin/src/app/app.component.ts
+++ app_admin/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TripListingComponent } from './trip-listing/trip-listing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TripListingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'travlr-admin';
+  title = 'Travlr Getaways Admin!';
 }
