7daef68cc49c0df5556d3fdf1b52cd5242781c09
cristiano-oss <cristiano.mirande@snhu.edu>
2026-08-11 16:24:17 -0700

    Added login logic to the admin SPA

--- app_admin/src/app/app.routes.ts
+++ app_admin/src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { AddTripComponent } from './add-trip/add-trip.component';
 import { TripListingComponent } from './trip-listing/trip-listing.component';
 import { EditTripComponent } from './edit-trip/edit-trip.component';
 import { RemoveTripComponent } from './remove-trip/remove-trip.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [
   { path: 'add-trip', component: AddTripComponent },
   { path: 'edit-trip', component: EditTripComponent },
   { path: 'remove-trip', component: RemoveTripComponent },
+  { path: 'login', component: LoginComponent },
   { path: '', component: TripListingComponent, pathMatch: 'full' },
 ];
--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,44 +1,80 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import { User } from '../models/user';
+import { AuthResponse } from '../models/auth-response';
+import { BROWSER_STORAGE } from '../storage';
 import { Trip } from '../models/trip';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TripDataService {
-  url = "http://localhost:3000/api/trips"
+  baseUrl = 'http://localhost:3000/api';
+  url = 'http://localhost:3000/api/trips';
 
-  constructor(private http: HttpClient) { }
+  constructor(
+    private http: HttpClient,
+    @Inject(BROWSER_STORAGE) private storage: Storage,
+  ) {}
 
-  getTrips() : Observable<Trip[]> {
+  // Call to our /login endpoint, returns JWT
+  login(user: User, passwd: string): Observable<AuthResponse> {
+    // console.log('Inside TripDataService::login');
+    return this.handleAuthAPICall('login', user, passwd);
+  }
+
+  // Call to our /register endpoint, creates user and returns JWT
+  register(user: User, passwd: string): Observable<AuthResponse> {
+    // console.log('Inside TripDataService::register');
+    return this.handleAuthAPICall('register', user, passwd);
+  }
+  
+  // helper method to process both login and register methods
+  handleAuthAPICall(
+    endpoint: string,
+    user: User,
+    passwd: string,
+  ): Observable<AuthResponse> {
+    // console.log('Inside TripDataService::handleAuthAPICall');
+    let formData = {
+      name: user.name,
+      email: user.email,
+      password: passwd,
+    };
+    return this.http.post<AuthResponse>(
+      this.baseUrl + '/' + endpoint,
+      formData,
+    );
+  }
+
+  getTrips(): Observable<Trip[]> {
     console.log('Inside TripDataService::getTrips');
 
     return this.http.get<Trip[]>(this.url);
   }
 
-  addTrip(formData: Trip) : Observable<Trip> {
+  addTrip(formData: Trip): Observable<Trip> {
     console.log('Inside TripDataService::addTrip');
 
-    return this.http.post<Trip>(this.url, formData)
+    return this.http.post<Trip>(this.url, formData);
   }
 
-  getTrip(tripCode: string) : Observable<Trip[]> {
+  getTrip(tripCode: string): Observable<Trip[]> {
     console.log('Inside TripDataService::getTrip');
-    
+
     return this.http.get<Trip[]>(this.url + '/' + tripCode);
   }
 
-  updateTrip(formData: Trip) : Observable<Trip[]> {
+  updateTrip(formData: Trip): Observable<Trip[]> {
     console.log('Inside TripDataService::updateTrip');
 
     return this.http.put<Trip[]>(this.url + '/' + formData.code, formData);
   }
-  
-  deleteTrip(tripCode: string) : Observable<Trip[]> {
+
+  deleteTrip(tripCode: string): Observable<Trip[]> {
     console.log('Inside TripDataService::deleteTrip');
-    
+
     return this.http.delete<Trip[]>(this.url + '/' + tripCode);
   }
 }
